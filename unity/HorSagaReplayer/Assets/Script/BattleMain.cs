bc7069429e311b56838565663cfdf792d9336b63
pepeta <pepeta0307@gmail.com>
2015-07-12 19:50:36 +0900

    タイトル画面とバトル画面背景の準備

---
+++ unity/HorSagaReplayer/Assets/Script/BattleMain.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Text;
+using System;
+
+
+public class BattleMain : MonoBehaviour {
+
+	private string guitxt = "";
+
+	// Use this for initialization
+	void Start () {
+
+		//	データ読み込み
+		_readFile ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//	データ読み込
+	void _readFile()
+	{
+		// FileReadTest.txtファイルを読み込む
+		FileInfo fi = new FileInfo(Application.dataPath + "/" + "data.csv");
+		try {
+			// 一行毎読み込み
+			using (StreamReader sr = new StreamReader(fi.OpenRead(), Encoding.UTF8)){
+				guitxt = sr.ReadToEnd();
+			}
+		} catch (Exception /* e */ ){
+			// 改行コード
+			guitxt += SetDefaultText();
+		}
+	}
+
+	//	改行コード処理
+	string SetDefaultText(){
+		return "C#あ\n";
+	}
+
+}
---
+++ unity/HorSagaReplayer/Assets/Script/TitleMain.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleMain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log( "start" );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey (KeyCode.Return)) {
+			Application.LoadLevel( "battle" );
+
+		}
+	}
+}
